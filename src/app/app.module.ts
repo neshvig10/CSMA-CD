25a666e85f8b3015f0d7037a445f18247bb36483
neshvig10 <svigneshwaran1073@gmail.com>
2023-08-08 19:28:45 +0530

    routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+import { Page0Component } from './page0/page0.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component,
+    Page3Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '',component: Page0Component},
+      {path: 'interference',component: Page1Component},
+      {path: 'backoff',component: Page2Component},
+      {path: 'csmacd',component: Page3Component},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page0/page0.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page0',
+  templateUrl: './page0.component.html',
+  styleUrls: ['./page0.component.css']
+})
+export class Page0Component {
+
+}
