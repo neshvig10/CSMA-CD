e563ba36349c94ef41d13ed272b20a624cd804ea
neshvig10 <svigneshwaran1073@gmail.com>
2023-08-25 00:13:10 +0530

    page4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,16 @@ import { AppComponent } from './app.component';
 import { Page0Component } from './page0/page0.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
-import { Page3Component } from './page3/page3.component'
+import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
     Page2Component,
-    Page3Component
+    Page3Component,
+    Page4Component
   ],
   imports: [
     BrowserModule,
